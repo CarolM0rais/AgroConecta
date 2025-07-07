ecfec30a538e17f9b74ac4c45a2e0a9094024005
CarolM0rais <caroldemorais07@gmail.com>
2025-07-07 15:39:51 -0300

    AgroConecta1

--- config/settings.py
+++ config/settings.py
@@ -75,8 +75,13 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    'ENGINE': 'django.db.backends.postgresql',
+    'NAME': 'AgroConecta',
+    'USER': 'postgres',
+    'PASSWORD': '123456',
+    'HOST': 'localhost',
+    'PORT': '5432',
+
     }
 }
 
